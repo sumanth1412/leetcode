58d8a394e43a1dba654eb3c67abd12fa5814e8e6
sumanth <sumanthsai817@gmail.com>
2025-01-21 17:31:39 -0500

    Jewels

---
+++ jewels.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+     Set<Character> jewelSet=new HashSet<>();
+     for(int i=0;i<jewels.length();i++){
+        jewelSet.add(jewels.charAt(i));
+     }  
+     int count=0;
+     for(int i=0;i<stones.length();i++){
+        if(jewelSet.contains(stones.charAt(i))){
+            count++;
+        }
+     }
+     return count;  
+    }
+}
